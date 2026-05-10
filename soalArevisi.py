a308ecfc306062bf25254a8d53dc40093c8a492e
rizkiputra27 <saputararizkidwi04@gmail.com>
2026-05-10 20:15:38 +0700

    latihan

--- soalArevisi.py
+++ soalArevisi.py
@@ -44,28 +44,43 @@ def hitung(kGedung,lama,jam):
 
 
 while True:
-    print("\nMasukkan detail transaksi:")
+    os.system('cls')
+    print("="*50)
+    print(f"|{'KODE':^5}|{'NAMA':^20}|{'SEWA':^10}|{'JAM':^10}|")
+    print("="*50)
+    for key,data in data_gedung.items():
+
+        NAMA  = data['nama']
+        SEWA = data['sewa']
+        JAM = data['jam']
+
+        print(f"|{key:^5}|{NAMA:^20}|{SEWA:^10}|{JAM:^10}|")
+    print("="*50)
+
+    print("\n=====Masukkan detail transaksi=====")
     nama=input("Masukkan nama penyewa: ")
     kGedung = input("Masukkan kode gedung: ")
     lama = int(input("Masukkan lama sewa: "))
     jam = int(input("Masukkan kelebihan jam: "))
 
-    print(data_gedung['rpt']['nama'])
 
     total3,diskon,totDis = hitung(kGedung,lama,jam)
     if total3 == 0 and diskon == 0 and totDis == 0:
-        print("Kode tidak ditemukan")
+        print()
+        print("=====Kode tidak ditemukan=====")
         print()
         print("="*117)
         print(f"|{"Nama":^25}|{"Nama Gedung":^25}|{"Lama Sewa":^15}|{"Harga awal":^15}|{"Diskon":^15}|{"Total":^15}|")
         print("="*117)
         print(f"|{nama:^25}|{"Gedung belum ada":^25}|{"-":^15}|{rupiah(total3):^15}|{rupiah(diskon):^15}|{rupiah(totDis):^15}|")
         print("="*117)
+        input("ENTER...")
     elif isinstance(total3,(int,float)) and isinstance(diskon,(int,float)) and isinstance(totDis,(int,float)):
         print("="*117)
         print(f"|{"Nama":^25}|{"Nama Gedung":^25}|{"Lama Sewa":^15}|{"Harga awal":^15}|{"Diskon":^15}|{"Total":^15}|")
         print("="*117)
         print(f"|{nama:^25}|{data_gedung[kGedung]['nama']:^25}|{lama:^15}|{rupiah(total3):^15}|{rupiah(diskon):^15}|{rupiah(totDis):^15}|")
         print("="*117)
+        input("ENTER...")
     else:
         print("Kode tidak ditemukan")
